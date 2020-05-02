f0187eba4b6d200f66d1bb177db753f643865be2
bgilbarreto <bgilbarreto@gmail.com>
2020-05-02 17:08:34 -0500

    correcciones 1

--- app/src/main/java/com/proyecto/androidjvapp/MainActivity.java
+++ app/src/main/java/com/proyecto/androidjvapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.proyecto.androidjvapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText str1, str2, str3;
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         clearOnClickEfecty();
         clearOnClickNumber();
         clearOnClickReference();
+        nextWindow();
 
     }
 
@@ -54,6 +59,17 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void nextWindow () {
+        btn1 = (Button) findViewById(R.id.button2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, camera_view.class));
+            }
+        });
+
     }
 
 }
