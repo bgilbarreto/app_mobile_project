4eaf5fbb1264cda316ced740447c3fe9b371ad92
bgilbarreto <bgilbarreto@gmail.com>
2020-05-02 18:25:45 -0500

    Diseño segunda pantalla

--- app/src/main/java/com/proyecto/androidjvapp/MainActivity.java
+++ app/src/main/java/com/proyecto/androidjvapp/MainActivity.java
@@ -17,48 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        clearOnClickEfecty();
-        clearOnClickNumber();
-        clearOnClickReference();
         nextWindow();
 
-    }
-
-    private void clearOnClickEfecty () {
-        str1 = (EditText) findViewById(R.id.Efecty);
-        str1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!str1.getText().equals("")) {
-                    str1.setText("");
-                }
-            }
-        });
-    }
-
-    private void clearOnClickNumber () {
-        str2 = (EditText) findViewById(R.id.Number);
-        str2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!str2.getText().equals("")) {
-                    str2.setText("");
-                }
-            }
-        });
-    }
-
-    private void clearOnClickReference () {
-        str3 = (EditText) findViewById(R.id.reference);
-        str3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!str3.getText().equals("")) {
-                    str3.setText("");
-                }
-            }
-        });
     }
 
     public void nextWindow () {
