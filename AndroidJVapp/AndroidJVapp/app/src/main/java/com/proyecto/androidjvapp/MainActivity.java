db979cc3d6c8156a61458ab0fdbbb3556078fab5
johansR22 <johan.rodriguezr@utadeo.edu.co>
2020-05-04 17:11:43 -0500

    segunda prueba de push

---
+++ AndroidJVapp/AndroidJVapp/app/src/main/java/com/proyecto/androidjvapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.proyecto.androidjvapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
