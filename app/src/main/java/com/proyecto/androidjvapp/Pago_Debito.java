887df6d76f52aade2cbc8792dbe88b87ff00a836
bgilbarreto <bgilbarreto@gmail.com>
2020-05-04 08:06:35 -0500

    Mini Programa de navegacion 1

--- app/src/main/java/com/proyecto/androidjvapp/Pago_Debito.java
+++ app/src/main/java/com/proyecto/androidjvapp/Pago_Debito.java
@@ -2,13 +2,32 @@ package com.proyecto.androidjvapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Pago_Debito extends AppCompatActivity {
+
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pago__debito);
+
+        nextWindow();
+
     }
+
+    public void nextWindow() {
+        btn1 = (Button) findViewById(R.id.confirm);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Pago_Debito.this, send_image.class));
+            }
+        });
+    }
+
 }
