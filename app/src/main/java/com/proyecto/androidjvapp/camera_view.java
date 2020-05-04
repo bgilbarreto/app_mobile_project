219eaf460675ba9b76ceea6b1fbb94b89067a6a3
bgilbarreto <bgilbarreto@gmail.com>
2020-05-03 20:32:49 -0500

    Pantalla de envio confirmado

--- app/src/main/java/com/proyecto/androidjvapp/camera_view.java
+++ app/src/main/java/com/proyecto/androidjvapp/camera_view.java
@@ -2,13 +2,32 @@ package com.proyecto.androidjvapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class camera_view extends AppCompatActivity {
+
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_view);
+
+        nextWindow();
+
+    }
+
+    public void nextWindow() {
+        btn1 = (Button) findViewById(R.id.camera_Button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(camera_view.this, confirm_delivery.class));
+            }
+        });
+
     }
 }
