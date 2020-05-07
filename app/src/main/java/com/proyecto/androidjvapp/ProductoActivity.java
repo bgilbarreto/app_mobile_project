d102f5d33927c4e4a3ad8d4f95b789ecfff15e90
josedavid0511 <josedavid0511@hotmail.com>
2020-05-07 17:19:09 -0500

    CatalogoActView

--- app/src/main/java/com/proyecto/androidjvapp/ProductoActivity.java
+++ app/src/main/java/com/proyecto/androidjvapp/ProductoActivity.java
@@ -3,12 +3,32 @@ package com.proyecto.androidjvapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ProductoActivity extends AppCompatActivity {
 
+    private Button btnAgregar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_catalogo);
+        setContentView(R.layout.activity_producto);
+
+        btnAgregar = findViewById(R.id.button);
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Proceso completado
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getBaseContext(), "Producto agregado", Toast.LENGTH_SHORT).show();
+                    }
+                }, 4000);
+            }
+        });
     }
 }
