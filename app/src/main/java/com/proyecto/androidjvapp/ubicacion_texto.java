9b8ef6261a94971c9d6bd7efb4df16a493eea47c
bgilbarreto <bgilbarreto@gmail.com>
2020-05-21 09:05:34 -0500

    Segunda pantalla de gps

---
+++ app/src/main/java/com/proyecto/androidjvapp/ubicacion_texto.java
@@ -0,0 +1,14 @@
+package com.proyecto.androidjvapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ubicacion_texto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubicacion_texto);
+    }
+}
