d4c9812141071d5ac8b5204758008ecafaeb8d58
bgilbarreto <bgilbarreto@gmail.com>
2020-05-26 21:46:24 -0500

    por fin funcionara la correcioin?

--- app/src/main/java/com/proyecto/androidjvapp/CatalogoActivity.java
+++ app/src/main/java/com/proyecto/androidjvapp/CatalogoActivity.java
@@ -17,19 +17,22 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.net.Uri;
+import android.content.pm.PackageManager;
+import android.content.Context;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class CatalogoActivity extends AppCompatActivity {
 
-    private Button btnFb,btnWa, btnIg, ver_ub;
-    private ImageView imProducto;
+    private Button btnFb,btnWa, btnIg, ver_ub, btn8;
+    private ImageView imProducto, imglogo;
+
+    private ProgressDialog progressDialog;
     private static final int REC_CODE_SPEECH_INPUT = 100;
     private TextView mEntradaVoz;
     private ImageButton mBotonHablar;
-
-    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +40,12 @@ public class CatalogoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_catalogo);
 
         nextWindow();
+        nextWQuestions();
 
         btnFb = findViewById(R.id.buttonFacebook);
         btnIg = findViewById(R.id.buttonInstagram);
         btnWa = findViewById(R.id.buttonWhatsapp);
+        btn8 = findViewById(R.id.button8);
 
         imProducto = findViewById(R.id.imageViewProducto);
         imProducto.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +55,14 @@ public class CatalogoActivity extends AppCompatActivity {
                 startActivity(intencion);
             }
         });
-
+        imglogo = findViewById(R.id.imageView5);
+        imglogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent updateperfil = new Intent(getApplication(), DatosBasicos.class);
+                startActivity(updateperfil);
+            }
+        });
 
 
         btnFb.setOnClickListener(new View.OnClickListener() {
@@ -64,9 +76,14 @@ public class CatalogoActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         Toast.makeText(getBaseContext(), "Conexión establecida", Toast.LENGTH_SHORT).show();
+                        //intent facebook
+                        Intent facebookIntent = new Intent(Intent.ACTION_VIEW);
+                        String facebookUrl = getFacebookPageURL(getBaseContext());
+                        facebookIntent.setData(Uri.parse(facebookUrl));
+                        startActivity(facebookIntent);
                         progressDialog.dismiss();
                     }
-                }, 4000);
+                }, 1000);
             }
         });
 
@@ -75,15 +92,18 @@ public class CatalogoActivity extends AppCompatActivity {
             public void onClick(View v) {
                 progresdialog();
                 showPDialogIG();
-
+                Uri uri = Uri.parse("http://instagram.com/_u/johan_s_85");
                 //Proceso completado
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         Toast.makeText(getBaseContext(), "Conexión establecida", Toast.LENGTH_SHORT).show();
+                        Intent intentIg = new Intent(Intent.ACTION_VIEW, uri);
+                        intentIg.setPackage("com.instagram.android");
+                        startActivity(intentIg);
                         progressDialog.dismiss();
                     }
-                }, 4000);
+                }, 1000);
             }
         });
 
@@ -98,13 +118,14 @@ public class CatalogoActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         Toast.makeText(getBaseContext(), "Conexión establecida", Toast.LENGTH_SHORT).show();
+                        openWhatsApp();
                         progressDialog.dismiss();
                     }
-                }, 4000);
+                }, 1000);
             }
         });
 
-        mEntradaVoz = findViewById(R.id.textoEntrada);
+        mEntradaVoz = findViewById(R.id.editText3);
         mBotonHablar = findViewById(R.id.botonHablar);
 
         mBotonHablar.setOnClickListener(new View.OnClickListener() {
@@ -117,19 +138,34 @@ public class CatalogoActivity extends AppCompatActivity {
 
     }
 
-    //Speech to text
-    private void iniciarEntradaVoz(){
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hola, di lo que quieras buscar");
-        try {
-            startActivityForResult(intent, REC_CODE_SPEECH_INPUT);
-        }catch (ActivityNotFoundException e){
+    //abrir Facebook
+    public static String FACEBOOK_URL = "https://www.facebook.com/Johans-Prote%C3%ADnas-y-vitaminas-1738612086383388/";
+    public static String FACEBOOK_PAGE_ID = "1738612086383388";
 
+    public String getFacebookPageURL(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        try {
+            int versionCode = packageManager.getPackageInfo("com.facebook.katana", 0).versionCode;
+            if (versionCode >= 3002850) { //newer versions of fb app
+                return "fb://facewebmodal/f?href=" + FACEBOOK_URL;
+            } else { //older versions of fb app
+                return "fb://page/" + FACEBOOK_PAGE_ID;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            return FACEBOOK_URL; //normal web url
         }
     }
 
+    //abrir whatsapp
+    private void openWhatsApp() {
+        String smsNumber = "573133279919";
+        Intent sendIntent = new Intent(Intent.ACTION_SEND);
+        sendIntent.setType("text/plain");
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Hola, estoy interesad@ en tus productos.");
+        sendIntent.putExtra("jid", smsNumber + "@s.whatsapp.net");
+        sendIntent.setPackage("com.whatsapp");
+        startActivity(sendIntent);
+    }
 
     private void progresdialog(){
         this.progressDialog =  new ProgressDialog(this);
@@ -168,6 +204,30 @@ public class CatalogoActivity extends AppCompatActivity {
                 startActivity(new Intent(CatalogoActivity.this, ubicacion_1.class));
             }
         });
+    }
+
+
+    public void nextWQuestions () {
+
+        btn8 = (Button) findViewById(R.id.button8);
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CatalogoActivity.this, Preguntas.class));
+            }
+        });
+    }
+
+    private void iniciarEntradaVoz(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hola, di lo que quieras buscar");
+        try {
+            startActivityForResult(intent, REC_CODE_SPEECH_INPUT);
+        }catch (ActivityNotFoundException e){
+
+        }
     }
 
     @Override
