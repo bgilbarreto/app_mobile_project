7cfbd4138a6661d8adf40176a09516669c76ada7
bgilbarreto <bgilbarreto@gmail.com>
2020-05-29 12:36:33 -0500

    navegacion complementaria

--- app/src/main/java/com/proyecto/androidjvapp/CarroDeCompras.java
+++ app/src/main/java/com/proyecto/androidjvapp/CarroDeCompras.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class CarroDeCompras extends AppCompatActivity {
@@ -18,6 +19,7 @@ public class CarroDeCompras extends AppCompatActivity {
     };
 
     int[] datosImg = {R.drawable.pro,R.drawable.proteinas,R.drawable.vit};
+    private Button pagar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +29,20 @@ public class CarroDeCompras extends AppCompatActivity {
         lista = (ListView) findViewById(R.id.lvCompras);
 
         lista.setAdapter(new Adaptador(this,datos,datosImg));
+        nextWindow();
 
     }
+
+    public void nextWindow () {
+        pagar = (Button) findViewById(R.id.pagoSiguiente);
+        pagar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CarroDeCompras.this, Pago.class));
+            }
+        });
+    }
+
     public void  siguinte(View View) {
         Intent siguinte = new Intent(this, envio.class);
         startActivity(siguinte);
--- app/src/main/java/com/proyecto/androidjvapp/envio.java
+++ app/src/main/java/com/proyecto/androidjvapp/envio.java
@@ -2,13 +2,32 @@ package com.proyecto.androidjvapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class envio extends AppCompatActivity {
+
+    private Button pagar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_envio);
+
+        nextWindow();
+
     }
+
+    public void nextWindow () {
+        pagar = (Button) findViewById(R.id.pagoVista);
+        pagar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(envio.this, ubicacion_texto.class));
+            }
+        });
+    }
+
 }
