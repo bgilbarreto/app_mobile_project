b6b9a111e64575f14beea17740cf45da5961ef96
david mejia <dandymejia4@gmail.com>
2020-05-29 18:01:15 -0500

    permisoscamara

--- app/src/main/java/com/proyecto/androidjvapp/camera_view.java
+++ app/src/main/java/com/proyecto/androidjvapp/camera_view.java
@@ -1,9 +1,13 @@
 package com.proyecto.androidjvapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -23,12 +27,17 @@ public class camera_view extends AppCompatActivity {
     private Button btn1;
     private ImageView img;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_view);
 
         //nextWindow();
+        img =(ImageView)findViewById(R.id.imageView);
+        if (ContextCompat.checkSelfPermission(camera_view.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(camera_view.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(camera_view.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1000);
+        }
 
     }
 
@@ -50,14 +59,15 @@ public class camera_view extends AppCompatActivity {
         });
     }
 
-    String currentPhotoPath;
+    String mCurrentPhotoPath;
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "Backup_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(imageFileName,".jpg",storageDir);
 
-        currentPhotoPath = image.getAbsolutePath();
+        //Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
         return image;
     }
 
